539351cd498fe4180d8fd618adbeeb2252c5b935
Jahsminemma <jahsminemma@gmail.com>
2021-11-15 07:24:55 -0500

    initial commit

---
+++ src/main/java/com/samaras/employeeMNGT/EmployeeMngtApplication.java
@@ -0,0 +1,13 @@
+package com.samaras.employeeMNGT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeMngtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeMngtApplication.class, args);
+	}
+
+}
